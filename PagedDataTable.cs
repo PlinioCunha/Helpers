aeed03767cab179cf37232c0760f3841613d924d
Plínio Cunha <pliniocunha@hotmail.com>
2017-04-28 15:14:01 -0300

    DataTable JS
    Retorna class para DataTable JS, paginado e ordenado.

---
+++ PagedDataTable.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Core.Data;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Core.Helpers
+{
+    public class DataTable<Tview>
+    {
+        public DataTable()
+        {
+            this.data = new List<Tview>();
+        }
+        public int draw { get; set; }
+        public int recordsTotal { get; set; }
+        public int recordsFiltered { get; set; }
+        public List<Tview> data { get; set; }
+    }
+
+    public interface IPagedDataTable<Tview, Tdomain>
+    {
+        DataTable<Tview> retornaDataTableJS(IGenericRepository<Tdomain> _repo, int start, int size, int draw,
+            string coluna,
+            bool orderbydesc,
+            Expression<Func<Tdomain, bool>> predicate);
+    }
+
+    public class PagedDataTable<Tview, Tdomain> : IPagedDataTable<Tview, Tdomain>
+    {
+        DataTable<Tview> IPagedDataTable<Tview, Tdomain>.retornaDataTableJS(IGenericRepository<Tdomain> _repo, int start, int size, int draw, 
+            string coluna, bool orderbydesc, Expression<Func<Tdomain, bool>> predicate)
+        {
+            var list = _repo.SearchFor(coluna, orderbydesc, predicate).ToPagedList((start == 0 ? 1 : (start / size) * 1), size);
+            var listView = Mapper.Map<List<Tdomain>, List<Tview>>(list.ToList());
+
+            DataTable<Tview> dt = new DataTable<Tview>();
+            dt.draw = draw;
+            dt.recordsFiltered = list.TotalItemCount;
+            dt.recordsTotal = list.TotalItemCount;
+            dt.data = listView;
+
+            return dt;
+        }      
+    }
+}
